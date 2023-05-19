690a617fc16ffeb34182e5e676688f8dc5e0c8fd
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2023-05-19 17:53:57 +0100

    first commit

---
+++ MathPowerLibraryStatic/MathPower.cpp
@@ -0,0 +1,13 @@
+// פאיכ MathPower.cpp
+#include "MathPower.h"
+namespace MathPower
+{
+	int Calculator::Sum(int a, int b)
+	{
+		return a + b;
+	}
+	int Calculator::Sub(int a, int b)
+	{
+		return a - b;
+	}
+}
---
+++ MathPowerLibraryStatic/MathPower.h
@@ -0,0 +1,12 @@
+#pragma once
+// פאיכ MathPower.h
+#pragma once
+namespace MathPower
+{
+	class Calculator
+	{
+	public:
+		int Sum(int a, int b);
+		int Sub(int a, int b);
+	};
+}
